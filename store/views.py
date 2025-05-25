7523190067dbe6c9afbdf26c48be6003864175ed
henry shyllon <henry@frobarn.com>
2025-05-24 22:01:14 -0500

    check if user has purchased product before submitting review

--- store/views.py
+++ store/views.py
@@ -10,6 +10,7 @@ from django.contrib import messages
 from carts.models import CartItem
 from category.models import Category
 from carts.views import _cart_id
+from orders.models import OrderProduct
 from .forms import ReviewForm
 from .models import Product, ReviewRating
 from .forms import ReviewForm
@@ -47,9 +48,15 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
     context = {
         'single_product': single_product,
-        'in_cart': in_cart
+        'in_cart': in_cart,
+        'orderproduct': orderproduct,
     }
 
     return render(request, 'store/product_detail.html', context)
