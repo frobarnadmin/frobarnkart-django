a055d524545aa6cec06894317768bc31fea8381d
henry shyllon <henry@frobarn.com>
2025-05-28 23:55:32 -0500

    complete product gallery on product detail page - unlimited product images

--- store/views.py
+++ store/views.py
@@ -12,7 +12,7 @@ from category.models import Category
 from carts.views import _cart_id
 from orders.models import OrderProduct
 from .forms import ReviewForm
-from .models import Product, ReviewRating
+from .models import Product, ReviewRating, ProductGallery
 from .forms import ReviewForm
 
 
@@ -59,11 +59,15 @@ def product_detail(request, category_slug, product_slug):
     # get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    # get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
 
     return render(request, 'store/product_detail.html', context)
