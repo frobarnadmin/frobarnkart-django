5580e6a03e9d24dcb74836073aa589e28a0c96e1
henry shyllon <henry@frobarn.com>
2025-10-09 21:29:01 -0500

    Updating email config in settings.py

--- frobarnkart/settings.py
+++ frobarnkart/settings.py
@@ -199,20 +199,21 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Where the email comes from
-DEFAULT_FROM_EMAIL = "admin@frobarn.com"
+
 
 # SMTP Configuration
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_PORT = config('EMAIL_PORT', cast=int)
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gshyllon@gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'lpsxlwojgnqzeqsb'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="admin@frobarn.com")
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'gshyllon@gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_PASSWORD = 'lpsxlwojgnqzeqsb'
 
 SITE_NAME = "Frobarn"
